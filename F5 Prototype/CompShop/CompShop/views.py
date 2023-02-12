bc926d1c0d8f50d05785659e6ee6b411afca5a01
Andrej Shumkovski <85301264+Shume00@users.noreply.github.com>
2023-02-12 23:14:14 +0100

    Product listing and visual styling
    - listing products on store page
    - cart page mocked up
    - switched navbar to dark mode
    - added support for bootstrap icons

--- F5 Prototype/CompShop/CompShop/views.py
+++ F5 Prototype/CompShop/CompShop/views.py
@@ -1,19 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
+
 
 def index(request):
     context = {}
     return render(request, 'CompShop/main.html', context)
 
+
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    productimages = ProductImages.objects.all()
+    context = {'products': products,
+               'productimages': productimages}
     return render(request, 'CompShop/store.html', context)
+
 
 def cart(request):
     context = {}
     return render(request, 'CompShop/cart.html', context)
 
+
 def checkout(request):
     context = {}
     return render(request, 'CompShop/checkout.html', context)
-
